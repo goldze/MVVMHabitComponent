0068fa0c0ab99eed6abcceb6a26d66c662cd8f2b
goldze <goldze@126.com>
2018-09-29 10:56:45 +0800

    supplement
    supplement

--- module-main/src/main/java/com/goldze/main/ui/adapter/ViewPagerBindingAdapter.java
+++ module-main/src/main/java/com/goldze/main/ui/adapter/ViewPagerBindingAdapter.java
@@ -3,6 +3,7 @@ package com.goldze.main.ui.adapter;
 import android.databinding.Observable;
 import android.databinding.ViewDataBinding;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.goldze.main.databinding.ItemViewpagerBinding;
 import com.goldze.main.ui.viewmodel.ViewPagerItemViewModel;
@@ -16,20 +17,29 @@ import me.tatarka.bindingcollectionadapter2.BindingViewPagerAdapter;
  */
 
 public class ViewPagerBindingAdapter extends BindingViewPagerAdapter<ViewPagerItemViewModel> {
-    private ItemViewpagerBinding _binding;
 
     @Override
-    public void onBindBinding(ViewDataBinding binding, int variableId, int layoutRes, int position, ViewPagerItemViewModel item) {
+    public void onBindBinding(final ViewDataBinding binding, int variableId, int layoutRes, int position, ViewPagerItemViewModel item) {
         super.onBindBinding(binding, variableId, layoutRes, position, item);
         //这里强转成ViewPagerItemViewModel对应的ViewDataBinding，
-        _binding = (ItemViewpagerBinding) binding;
+        final ItemViewpagerBinding _binding = (ItemViewpagerBinding) binding;
         KLog.e("onBindBinding：" + _binding.tvContent.getText().toString());
-        item.uc.onClickObservable.addOnPropertyChangedCallback(new Observable.OnPropertyChangedCallback() {
+        /**
+         * 这个位置要注意,Observable是add一个Callback，如果页面不停的切换,onBindBinding会调用多次，所以会不停的add,
+         * 导致回调方法onPropertyChanged执行多次
+         * */
+        final Observable.OnPropertyChangedCallback callback = new Observable.OnPropertyChangedCallback() {
             @Override
             public void onPropertyChanged(Observable observable, int i) {
                 KLog.e("onClickObservable：" + _binding.tvContent.getText().toString());
                 ToastUtils.showShort(_binding.tvContent.getText().toString());
             }
-        });
+        };
+        item.uc.onClickObservable.addOnPropertyChangedCallback(callback);
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        super.destroyItem(container, position, object);
     }
 }
